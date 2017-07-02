ea0ff3dc13827813718ca3c883d71692b2d4f014
rayzalzero <rayzalzero@gmail.com>
2017-07-02 17:34:04 +0700

    io-naivebayes

--- demo.js
+++ demo.js
@@ -1,8 +1,8 @@
-const nbayes = require('./lib/app')
+const nbayes = require('./lib/io-naivebayes')
 
 
 nbayes.train('positif', 'budi alhamdulillah');
 nbayes.train('negatif', 'budi suka bom');
 
 console.log(nbayes.classify('alhamdulillah baik baik saja'));
-console.log(nbayes.classify('bom itu meledak'));
+console.log(nbayes.classifyplus('bom itu meledak'));
